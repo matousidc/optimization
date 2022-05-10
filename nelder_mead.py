3e48f23fda93a596a1be855f99928ce9744bbc32
Matouš <98881259+matousidc@users.noreply.github.com>
2022-05-10 09:33:01 +0200

    Update nelder_mead.py

--- nelder_mead.py
+++ nelder_mead.py
@@ -1,9 +1,12 @@
+"""
+How to run:
+python -m nelder_mead
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
-import gif
 
-
-def rosen(p: list) -> int:
+def rosen(p: list) -> int:              # rosenbrock test function
     x1, x2 = p
     a = 1
     b = 100
@@ -11,7 +14,7 @@ def rosen(p: list) -> int:
     return y
 
 
-def anim(u, v, w):
+def anim(u: list, v: list, w: list):        # plotting function
     x1 = np.arange(-5, 5, 0.1)
     x2 = np.arange(-5, 5, 0.1)
     x1, x2 = np.meshgrid(x1, x2)
@@ -62,75 +65,14 @@ def algo(points: list, func):
         if min([np.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[0]) ** 2) for x, y in zip([u, v, w], [w, u, v])]) < 0.05:
             check = 0  # size of simplex convergence
         anim(u, v, w)
-        plt.pause(0.1)
+        plt.pause(0.4)
         plt.clf()
         # print('f(x):', [func(x) for x in [u, v, w]])
         print('points:', [u, v, w])
-        final = [(u[x] + v[x] + w[x]) / 3 for x in range(2)]
-        print('final:', final)
+    final = [(u[x] + v[x] + w[x]) / 3 for x in range(2)]
+    print('final:', final)
 
 
-# algo([[2, -2], [3, -2], [3, -1]], rosen)
-# algo([[2, -2], [4, -2], [4, -4]], rosen)
+algo([[2, -2], [4, -2], [4, -4]], rosen)
 
 
-def dummy(points: list, func):
-    u, v, w = points
-    u, v, w = sorted([x for x in [u, v, w]], key=lambda k: func(k))
-    c = [(v[x] + u[x]) / 2 for x in range(len(u))]  # centroid
-    r = [c[x] + (c[x] - w[x]) for x in range(2)]  # reflect
-    if func(u) <= func(r) < func(v):  # accept reflect
-        w = r.copy()
-    elif func(r) < func(u):
-        e = [c[x] + 2 * (r[x] - c[x]) for x in range(2)]  # extend
-        if func(e) < func(r):  # accept extend
-            w = e.copy()
-        else:  # accept reflect
-            w = r.copy()
-    else:
-        if func(v) <= func(r) < func(w):  # contract outside
-            contr = [c[x] + 0.5 * (r[x] - c[x]) for x in range(2)]
-            if func(contr) <= func(r):
-                w = contr.copy()
-            else:  # shrink
-                v = [u[x] + 0.5 * (v[x] - u[x]) for x in range(2)]
-                w = [u[x] + 0.5 * (w[x] - u[x]) for x in range(2)]
-        else:  # contract inside
-            contr = [c[x] + 0.5 * (w[x] - c[x]) for x in range(2)]
-            if func(contr) < func(w):
-                w = contr.copy()
-            else:  # shrink
-                v = [u[x] + 0.5 * (v[x] - u[x]) for x in range(2)]
-                w = [u[x] + 0.5 * (w[x] - u[x]) for x in range(2)]
-    return [u, v, w]
-
-
-@gif.frame
-def dummy2(points: list):
-    u, v, w = points
-    x1 = np.arange(-5, 5, 0.1)
-    x2 = np.arange(-5, 5, 0.1)
-    x1, x2 = np.meshgrid(x1, x2)
-    ax = plt.axes()
-    return ax.imshow(rosen([x1, x2]), origin='lower', extent=[-5, 5, -5, 5], vmax=1000), \
-           ax.plot([u[0], v[0]], [u[1], v[1]], color='black', linewidth=2), \
-           ax.plot([v[0], w[0]], [v[1], w[1]], color='black', linewidth=2), \
-           ax.plot([u[0], w[0]], [u[1], w[1]], color='black', linewidth=2)
-
-
-frames = []
-check = 1
-points_list = [[2, -2], [4, -2], [4, -4]]
-frame = dummy2(points_list)
-frames.append(frame)
-while check == 1:
-    points_list = dummy(points_list, rosen)
-    frame = dummy2(points_list)
-    frames.append(frame)
-    plt.pause(0.4)
-    plt.clf()
-    u, v, w = points_list
-    if min([np.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[0]) ** 2) for x, y in zip([u, v, w], [w, u, v])]) < 0.05:
-        check = 0  # size of simplex convergence
-
-gif.save(frames, 'nelder_mead.gif', duration=8, unit='seconds', between="startend")
